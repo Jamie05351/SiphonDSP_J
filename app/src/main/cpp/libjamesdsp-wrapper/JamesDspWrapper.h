16fade32ea1f3ddc3c3255353f18b8be6377155f
Jamie05351 <james.e.hay05@gmail.com>
2026-07-24 00:00:27 +1000

    Remove sidecar PEQ state from JamesDSP wrapper

--- app/src/main/cpp/libjamesdsp-wrapper/JamesDspWrapper.h
+++ app/src/main/cpp/libjamesdsp-wrapper/JamesDspWrapper.h
@@ -43,7 +43,6 @@ struct JniEnvironmentHandle
 typedef struct
 {
     void* dsp;
-    void* nativePeq;
     JniEnvironmentHandle env;
     jobject callbackInterface;
     jmethodID callbackOnLiveprogOutput;
