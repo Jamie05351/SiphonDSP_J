71b382dab330a23386101f4e8022422a7af85a3b
Jamie05351 <james.e.hay05@gmail.com>
2026-07-24 22:31:15 +1000

    Expose native BMW DSP controls to Kotlin

--- app/src/main/java/app/siphondsp/interop/JamesDspWrapper.kt
+++ app/src/main/java/app/siphondsp/interop/JamesDspWrapper.kt
@@ -6,27 +6,23 @@ import app.siphondsp.model.ProcessorMessage
 typealias JamesDspHandle = Long
 
 object JamesDspWrapper {
-    // Memory management
     external fun alloc(callbacks: JamesDspCallbacks): JamesDspHandle
     external fun free(self: JamesDspHandle)
     external fun isHandleValid(self: JamesDspHandle): Boolean
 
-    // Benchmarking
     external fun getBenchmarkSize(): Int
     external fun runBenchmark(c0: DoubleArray, c1: DoubleArray)
     external fun loadBenchmark(c0: DoubleArray, c1: DoubleArray)
 
-    // Processing (interleaved)
     external fun processInt16(self: JamesDspHandle, input: ShortArray, output: ShortArray, offset: Int = -1, length: Int = -1)
     external fun processInt8U24(self: JamesDspHandle, input: IntArray): IntArray
     external fun processInt24Packed(self: JamesDspHandle, input: BooleanArray): BooleanArray
     external fun processInt32(self: JamesDspHandle, input: IntArray, output: IntArray, offset: Int = -1, length: Int = -1)
     external fun processFloat(self: JamesDspHandle, input: FloatArray, output: FloatArray, offset: Int = -1, length: Int = -1)
 
-    // Engine config
     external fun setSamplingRate(self: JamesDspHandle, sampleRate: Float, forceRefresh: Boolean)
+    external fun configureNativeBmwDsp(values: FloatArray): Boolean
 
-    // Effect config
     external fun setLimiter(self: JamesDspHandle, threshold: Float, release: Float): Boolean
     external fun setPostGain(self: JamesDspHandle, postGain: Float): Boolean
     external fun setMultiEqualizer(self: JamesDspHandle, enable: Boolean, filterType: Int, interpolationMode: Int, bands: DoubleArray): Boolean
@@ -41,15 +37,12 @@ object JamesDspWrapper {
     external fun setVacuumTube(self: JamesDspHandle, enable: Boolean, level: Float): Boolean
     external fun setLiveprog(self: JamesDspHandle, enable: Boolean, id: String, liveprogContent: String): Boolean
 
-    // EEL VM utilities
     external fun enumerateEelVariables(self: JamesDspHandle): ArrayList<EelVmVariable>
     external fun manipulateEelVariable(self: JamesDspHandle, name: String, value: Float): Boolean
     external fun freezeLiveprogExecution(self: JamesDspHandle, freeze: Boolean)
     external fun eelErrorCodeToString(errorCode: Int): String
 
-    // Callbacks
-    interface JamesDspCallbacks
-    {
+    interface JamesDspCallbacks {
         fun onLiveprogOutput(message: String)
         fun onLiveprogExec(id: String)
         fun onLiveprogResult(resultCode: Int, id: String, errorMessage: String?)
@@ -57,8 +50,7 @@ object JamesDspWrapper {
         fun onConvolverParseError(errorCode: ProcessorMessage.ConvolverErrorCode)
     }
 
-    init
-    {
+    init {
         System.loadLibrary("jamesdsp-wrapper")
     }
 }
